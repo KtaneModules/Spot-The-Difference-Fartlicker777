35eddd56c0fc29747ae4b53ca0be2cbceca55db3
Apolo Simon <62307787+Fartlicker777@users.noreply.github.com>
2022-04-25 20:43:25 -0500

    removed souv

--- Spot The Difference/Assets/SpotTheDifference.cs
+++ Spot The Difference/Assets/SpotTheDifference.cs
@@ -23,7 +23,7 @@ public class SpotTheDifference : MonoBehaviour
 
     private List<int> abcefgh = new List<int>{0,1,2,3,2,1,1,1,3,3,3,0,0,0,2,1,3,2,0,0,0,3,2,2,1,1,3,2,0,0,1,0,3,3,0,1,1,2,2,2,0,0,3,0,1,1,2,1,3,0,3,3,2,3,1,2,1,2,0,0};
     int hgnjmhgnjmgfnb = 0;
-    int jon = -1000000;
+    int _jon = -1000000;
     private Vector3 a = new Vector3(0f, 0.02f, 0f);
     private Vector3 b = new Vector3(0f, 0.06f, 0f);
     private Vector3 c = new Vector3(0f, 0.1f, 0f);
@@ -48,13 +48,13 @@ public class SpotTheDifference : MonoBehaviour
     void Start()
 	{
 		hgnjmhgnjmgfnb = UnityEngine.Random.Range(0, WeedChungi.Count());
-		jon = UnityEngine.Random.Range(0,4);
-		while (abcefgh[hgnjmhgnjmgfnb] == jon)
+		_jon = UnityEngine.Random.Range(0,4);
+		while (abcefgh[hgnjmhgnjmgfnb] == _jon)
 		{
-			jon = UnityEngine.Random.Range(0,4);
+			_jon = UnityEngine.Random.Range(0,4);
 		}
-		Debug.LogFormat("[Spot The Difference #{0}] The faulty sphere is {1} (Starting from the bottom row, then the bottom left sphere according to the image and going in geometric order starting from 1). The color of the sphere is {2}.", moduleId, hgnjmhgnjmgfnb+1, logcolor[jon]);
-		WeedChungi[hgnjmhgnjmgfnb].GetComponent<MeshRenderer>().material = Colors[jon];
+		Debug.LogFormat("[Spot The Difference #{0}] The faulty sphere is {1} (Starting from the bottom row, then the bottom left sphere according to the image and going in geometric order starting from 1). The color of the sphere is {2}.", moduleId, hgnjmhgnjmgfnb+1, logcolor[_jon]);
+		WeedChungi[hgnjmhgnjmgfnb].GetComponent<MeshRenderer>().material = Colors[_jon];
 		StartCoroutine(looper());
 		GetComponent<KMBombModule>().OnActivate += SpotTheDifferenceInTP;
 	}
